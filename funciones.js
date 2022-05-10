78adad140dd82f071efb4df5bfbd91ddc2ce39a3
Sergio H <sergiohernandezroman@gmail.com>
2022-05-10 12:56:27 +0200

    Add fuctions subtract and product

--- funciones.js
+++ funciones.js
@@ -1,3 +1,9 @@
 // funciones
 
-const suma = (a, b) => a + b;
+const add = (a, b) => a + b;
+const subtract = (a = 0, b = 0) => {
+    return a - b;
+};
+const prod = (a = 0, b = 0) => {
+    return a * b;
+};
